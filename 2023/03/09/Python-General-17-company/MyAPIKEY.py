bdc83971347f9d4caad6a9aa7e7c4e034e13cd45
Jehyun Lee <jehyun.lee@gmail.com>
2023-03-10 00:00:27 +0900

    Site updated: 2023-03-09 23:58:59

---
+++ 2023/03/09/Python-General-17-company/MyAPIKEY.py
@@ -0,0 +1,18 @@
+SerpAPI = "7f975513fa4e2b697aa4806fd6014de22a2c63550dedfbf45febb67f30512e94" #jehyunlee@kier.re.kr
+RapidAPI = 'bf5de0c77bmsh73631a5a20a8e93p1f69c9jsn5dac198e9395'   #jehyunlee@kier.re.kr
+NaverAPI_id = "31gtpnit4h"
+NaverAPI_secret = "vwC8w5iMEB2zGAHMOmu0RdgZuI0VySHmECv992cv"
+Naver_client_id = "3kcwvsptpy"  #jehyunlee@kier.re.kr
+Naver_client_secret = "i8syHzWfAnx9GowDouuG1k2I0E0w3Xo4WNROHY6P"
+ScopusAPIs = ["fe26c5b9ca0ae3918b85fa494a9c635e",
+           "967ac6c5db7b8b34c178035e0ea7e22c", 
+           "55eef3b02fc28c636e2fada035cf03d2", 
+           "e1fc56d1d9fcc86e6998a7fd79faed23", 
+           "dcbed1c3abb36a8e80addbf185f38671", 
+           "78d8e510bda00c5530f560f02bc051bc", 
+           "4b9ca0308c3ae9b650f74fd19d07d657", 
+           "8b02da5833a012844657fc27d129349f", 
+           "6624310bd1610368c82261bc6d437be4"]
+SemanticScholarS2 = "y5fdNkiWtE2hzV4DL80ynIz2VO7b81kakd4TRsy9"
+OpenAI = "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"  #jehyun.lee@gmail.com
+# OpenAPI = "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"  #jehyunlee.dev@gmail.com
