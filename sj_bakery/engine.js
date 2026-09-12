200272e0fc94180acd38ce389c77776e7d0bc953
Pega <jehyun.lee@gmail.com>
2026-09-13 08:04:36 +0900

    Slow each answer-reveal transform to 0.2 seconds

--- sj_bakery/engine.js
+++ sj_bakery/engine.js
@@ -1,7 +1,7 @@
 export const STAGE_SECONDS=60;
 export const QUESTION_SECONDS=5;
 export const getQuestionSeconds=stage=>QUESTION_SECONDS+Math.max(0,stage-1)*.5;
-export const REVEAL_STEP_SECONDS=.1;
+export const REVEAL_STEP_SECONDS=.2;
 export const COOKIE_SECONDS=QUESTION_SECONDS;
 export const QUESTIONS_PER_STAGE=20;
 export const COOKIES_PER_STAGE=QUESTIONS_PER_STAGE;
